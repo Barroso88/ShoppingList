1ce02aab0e9922955878a0988c418869de0dc3ee
André Barroso <andrebarroso@MacBook-Pro-Nenecos.local>
2026-05-17 03:26:31 +0100

    fix: Add allowedOrigins to Next.js config to fix CSRF issues with Server Actions behind Cloudflare Tunnels

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  serverActions: {
+    allowedOrigins: ['shopping.barrosoportal.com', 'localhost:3056', 'localhost:3000'],
   }
 };
 
